a1b8e0395f1d33e01c71045a170164c20a9e4d7e
Ismael Ramos <ramossilvanismael@gmail.com>
2017-02-21 18:22:08 +0100

    feat(home component): overflow and content in home component

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
+
+declare let $;
 
 @Component({
   selector: 'ir-home-page',
@@ -6,5 +8,16 @@ import {Component} from '@angular/core';
   styleUrls: ['home-page.component.scss']
 })
 
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
+  private greetings = ['Hello', 'Salut', 'Hallo', 'Ciao', 'Hola', 'Konnichiwa'];
+  private greeting: string;
+
+  constructor() {
+    this.greeting = this.greetings[Math.floor(Math.random() * this.greetings.length)];
+  }
+
+  ngAfterViewInit() {
+    $('#greeting').textillate({in: {delay: 50, effect: 'wobble'}});
+  }
+
 }
