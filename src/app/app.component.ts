94e484d6eb196a35d9af9f0615df5fc55c503a91
ismaestro <ramossilvanismael@gmail.com>
2017-03-01 22:10:23 +0100

    chore(test): test to deploy in gh-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import {TranslateService}  from 'ng2-translate';
 export class AppComponent {
   private translateService: TranslateService;
 
-
   constructor(translateService: TranslateService) {
     this.translateService = translateService;
     this.translateService.setDefaultLang('en');
