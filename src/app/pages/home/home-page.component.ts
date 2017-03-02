b23e3eee802238e24df8f22f1b0b617f30bd7faa
Ismael Ramos <ramossilvanismael@gmail.com>
2017-03-02 20:41:53 +0100

    feat(texts, styles): updated texts and improved styles

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, AfterViewInit} from '@angular/core';
 import {EasterEggService} from '../../shared/easter-egg.service';
+import {TranslateService} from 'ng2-translate';
 
 declare let $;
 
@@ -13,41 +14,51 @@ export class HomePageComponent implements AfterViewInit {
   private greetings = ['Hello!', 'Salut!', 'Hallo!', 'Hola!', 'Konnichiwa!', 'Hi!', 'Hey!'];
   greeting: string;
   randomLike: string;
-  labs: Array<any>;
+  projects: Array<any>;
 
-  constructor(private easterEggService: EasterEggService) {
+  constructor(private easterEggService: EasterEggService,
+              private translateService: TranslateService) {
     this.greeting = this.greetings[Math.floor(Math.random() * this.greetings.length)];
     let likes = ['electronicMusic', 'goToCinema', 'myGirlfriend', 'goCiclying',
       'goingToTheMountain', 'playingSquash', 'swimming', 'diving', 'coding', 'playVideogames',
       'driving', 'sleeping'];
 
-    this.randomLike = likes[Math.floor(Math.random() * likes.length)];
-    setInterval(() => {
-      this.randomLike = likes[Math.floor(Math.random() * likes.length)];
-    }, 3000);
+    this.loadLikes(likes);
 
-    this.labs = [
-      {
-        id: '001',
-        name: 'Web Peluqueria Pilar Silvan',
-        link: 'https://www.peluqueriapilarsilvan.com'
-      },
-      {
-        id: '002',
-        name: 'Packing up',
-        link: 'https://play.google.com/store/apps/details?id=packingup.core.activities&hl=es'
-      },
-      {
-        id: '003',
-        name: 'SECD',
-        link: 'https://sourceforge.net/projects/secd/'
-      },
-      {
-        id: '004',
-        name: 'Gijón Squash Club app',
-        link: 'https://play.google.com/store/apps/details?id=gijonsquashclub.liga'
-      },
-    ];
+    this.translateService.get(['PPSWebsite', 'PackingUpApp', 'GSCApp']).subscribe((texts) => {
+      this.projects = [
+        {
+          id: '001',
+          name: texts['PPSWebsite'],
+          link: 'https://www.peluqueriapilarsilvan.com'
+        },
+        {
+          id: '002',
+          name: texts['PackingUpApp'],
+          link: 'https://play.google.com/store/apps/details?id=packingup.core.activities&hl=es'
+        },
+        {
+          id: '003',
+          name: 'SECD',
+          link: 'https://sourceforge.net/projects/secd/'
+        },
+        {
+          id: '004',
+          name: texts['GSCApp'],
+          link: 'https://play.google.com/store/apps/details?id=gijonsquashclub.liga'
+        },
+      ];
+    });
+  }
+
+  loadLikes(likes) {
+    let offset = 0;
+    likes.forEach((like) => {
+      setTimeout(() => {
+        this.randomLike = like;
+      }, 2500 + offset);
+      offset += 2500;
+    });
   }
 
   ngAfterViewInit() {
